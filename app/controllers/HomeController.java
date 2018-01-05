9c634dbcfdc48a5de626e94bf08d3733fc0335eb
Ozgur <ozgurfiratcinar@gmail.com>
2018-01-06 02:24:57 +0300

    hierarchy edited

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -10,21 +10,11 @@ import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.*;
 
-/**
- * This controller contains an action to handle HTTP requests
- * to the application's home page.
- */
 public class HomeController extends Controller {
 
     @Inject
     FormFactory formFactory;
 
-    /**
-     * An action that renders an HTML page with a welcome message.
-     * The configuration in the <code>routes</code> file means that
-     * this method will be called when the application receives a
-     * <code>GET</code> request with a path of <code>/</code>.
-     */
     public Result loginSubmit() {
 
         DynamicForm dynamicForm = formFactory.form().bindFromRequest();
@@ -73,6 +63,7 @@ public class HomeController extends Controller {
 
         newUser.save();
 
+        session("connected", registerUsername);
         return ok(logged_in.render("User " + registerUsername + " registered successfully!"));
     }
 
