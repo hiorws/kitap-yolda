995a92263c93b7264ca3ef57da8c053e5ee95664
Ozan Onur TEK <ozanonurtek@gmail.com>
2018-01-06 23:36:44 +0300

    transition UI

--- test/UnitTest.java
+++ test/UnitTest.java
@@ -1,15 +1,6 @@
-import akka.actor.ActorSystem;
-import controllers.AsyncController;
-import controllers.CountController;
 import org.junit.Test;
-import play.mvc.Result;
-import scala.concurrent.ExecutionContextExecutor;
-
-import java.util.concurrent.CompletionStage;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.awaitility.Awaitility.await;
-import static play.test.Helpers.contentAsString;
 
 /**
  * Unit testing does not require Play application start up.
@@ -24,32 +15,5 @@ public class UnitTest {
         assertThat(a).isEqualTo(2);
     }
 
-    // Unit test a controller
-    @Test
-    public void testCount() {
-        final CountController controller = new CountController(() -> 49);
-        Result result = controller.count();
-        assertThat(contentAsString(result)).isEqualTo("49");
-    }
-
-    // Unit test a controller with async return
-    @Test
-    public void testAsync() {
-        final ActorSystem actorSystem = ActorSystem.create("test");
-        try {
-            final ExecutionContextExecutor ec = actorSystem.dispatcher();
-            final AsyncController controller = new AsyncController(actorSystem, ec);
-            final CompletionStage<Result> future = controller.message();
-
-            // Block until the result is completed
-            await().until(() -> {
-                assertThat(future.toCompletableFuture()).isCompletedWithValueMatching(result -> {
-                    return contentAsString(result).equals("Hi!");
-                });
-            });
-        } finally {
-            actorSystem.terminate();
-        }
-    }
 
 }
